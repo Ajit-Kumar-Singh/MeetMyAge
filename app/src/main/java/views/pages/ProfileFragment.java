ffc3463201d2ae62d32a07c9c645df6451f05180
Ajit Singh <ajit.sumitsingh@gmail.com>
2018-06-12 03:43:43 +0530

    Edit and save profile

--- app/src/main/java/views/pages/ProfileFragment.java
+++ app/src/main/java/views/pages/ProfileFragment.java
@@ -6,28 +6,46 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.meetmyage.com.meetmyageapp.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import data.ApiClient;
+import data.ApiInterface;
 import data.SessionManagementUtil;
+import data.model.Location;
 import data.model.Profile;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 // Managing the Profile in this class
 public class ProfileFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
+    private String TAG = ProfileFragment.class.getSimpleName();
+    private boolean isPreview = true;
 
     @BindView(R.id.userName) TextView mProfileName;
     @BindView(R.id.profileWork) TextView mProfileWork;
     @BindView(R.id.profileStory) TextView mProfileStory;
     @BindView(R.id.fab) FloatingActionButton mFab;
+    @BindView(R.id.layoutPreview) LinearLayout mLayoutPreview;
+    @BindView(R.id.layoutEdit) LinearLayout mLayoutEdit;
+    @BindView(R.id.saveProfile) Button saveProfile;
+    @BindView(R.id.editprofileName) EditText mEditProfileName;
+    @BindView(R.id.editprofileStory) EditText mEditProfileStrory;
+    @BindView (R.id.editprofileWork) EditText mEditProfileWork;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -48,23 +66,95 @@ public class ProfileFragment extends Fragment {
     }
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        Profile profile = SessionManagementUtil.getUserData();
-        mProfileName.setText(profile.getProfileName());
-        mProfileStory.setText(profile.getProfileStory());
-        mProfileWork.setText(profile.getProfileWork());
-
-        mFab.setOnClickListener(new View.OnClickListener() {
+        showPreviewPage();
+        view.setFocusableInTouchMode(true);
+        view.requestFocus();
+        view.setOnKeyListener(new View.OnKeyListener() {
             @Override
-            public void onClick(View view) {
-                FragmentTransaction trans = getFragmentManager()
-                        .beginTransaction();
-                trans.replace(R.id.root_frame, new EditProfileFragment());
-                trans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-                trans.addToBackStack(null);
-                trans.commit();
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                Log.i(TAG, "keyCode: " + keyCode);
+                if( keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    if (isPreview)
+                    {
+                        showPreviewPage();
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
+        }
+
+
+    private void showPreviewPage()
+    {
+        if (isPreview)
+        {
+            mLayoutEdit.setVisibility(View.GONE);
+            mLayoutPreview.setVisibility(View.VISIBLE);
+            //Set Initial data
+            Profile savedProfile = SessionManagementUtil.getUserData();
+            mProfileName.setText(savedProfile.getProfileName());
+            mProfileStory.setText(savedProfile.getProfileStory());
+            mProfileWork.setText(savedProfile.getProfileWork());
+            isPreview = false;
+            mFab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    showEditPage();
+                }
+            });
+
+        }
+    }
+
+    private void showEditPage()
+    {
+        if (!isPreview)
+        {
+            //Set Initial data
+            Profile savedProfile = SessionManagementUtil.getUserData();
+            mEditProfileName.setText(savedProfile.getProfileName());
+            mProfileStory.setText(savedProfile.getProfileStory());
+            mEditProfileWork.setText(savedProfile.getProfileWork());
+            mLayoutEdit.setVisibility(View.VISIBLE);
+            mLayoutPreview.setVisibility(View.GONE);
+            isPreview =  true;
+            saveProfile.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    updateDataToServer();
+                }
+            });
+        }
+    }
+    private void updateDataToServer()
+    {
+        //Update Data to server
+        ApiInterface apiService =
+                ApiClient.getClient().create(ApiInterface.class);
+
+        Call<Profile> call = null;
+        call = apiService.updateProfile(SessionManagementUtil.getUserData().getProfileId(),
+                new Profile(SessionManagementUtil.getUserData().getProfileId(),mEditProfileName.getText().toString(),mEditProfileStrory.getText().toString(),mEditProfileWork.getText().toString(),new Location()));
+
+        call.enqueue(new Callback<Profile>() {
+            @Override
+            public void onResponse(Call<Profile> call, Response<Profile> response) {
+                Profile responseProfile = response.body();
+                SessionManagementUtil.updateProfile(
+                        responseProfile.getProfileName(),"",responseProfile.getProfileStory(),responseProfile.getProfileWork(),new Location());
+                showPreviewPage();
+            }
+
+            @Override
+            public void onFailure(Call<Profile> call, Throwable t) {
+                // Log error here since request failed
+                Log.e(TAG, t.toString());
             }
         });
     }
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
