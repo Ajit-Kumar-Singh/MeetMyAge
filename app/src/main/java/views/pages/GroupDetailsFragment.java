d004f1e84dc77a50d1d1d31024637335ff2c98b4
uddas <UDAYAN.DAS@ORACLE.COM>
2018-06-28 15:51:50 +0530

    added members to recommended groups fragment

--- app/src/main/java/views/pages/GroupDetailsFragment.java
+++ app/src/main/java/views/pages/GroupDetailsFragment.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,22 +26,28 @@ import data.model.Group;
 
 public class GroupDetailsFragment extends Fragment {
 
+    LayoutInflater mInflater = null;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_group_details, container, false);
+        mInflater = inflater;
         ButterKnife.bind(this,view);
         addImages(view);
         populateDescription(view);
+        addMembers(view);
         return view;
     }
     private void populateDescription(View pView) {
         TextView myNameTextView = pView.findViewById(R.id.recommended_group_details_group_name);
         TextView myDescTextView = pView.findViewById(R.id.recommended_group_details_group_desc);
+        TextView myMembesTag = pView.findViewById(R.id.recommended_group_details_MembersTag);
         Group mySelectedGroup = SessionManagementUtil.getSelectedGroup();
         myNameTextView.setText(mySelectedGroup.getGroupName());
         myDescTextView.setText(mySelectedGroup.getGroupStory());
+        myMembesTag.setText("Members");
     }
     private void addImages(View pView) {
         List<MediaInfo> infos = new ArrayList<>();
@@ -54,9 +61,20 @@ public class GroupDetailsFragment extends Fragment {
         ScrollGalleryView scrollGalleryView = (ScrollGalleryView)pView.findViewById(R.id.scroll_gallery_view);
         scrollGalleryView
                 .setThumbnailSize(100)
-                .setZoom(true)
+                .setZoom(false)
                 .setFragmentManager(getFragmentManager())
                 .addMedia(infos);
 
+    }
+
+    private void addMembers(View pView) {
+        LinearLayout myLayout = pView.findViewById(R.id.recommendedGroupsMembers);
+        for (int i=1; i <=5; i++) {
+            View myView = mInflater.inflate(R.layout.fragment_group_members, null);
+            TextView mMemberName = myView.findViewById(R.id.group_members_name);
+            mMemberName.setText("Bharwa number "+i);
+            Log.i("BHARWA NO",""+i);
+            myLayout.addView(myView);
+        }
     }
 }
