a9c9f302cbe61e135dafc295619370b2c7b47284
uddas <UDAYAN.DAS@ORACLE.COM>
2018-07-09 13:38:20 +0530

    checkin for events page....need to improve ui

--- app/src/main/java/views/pages/EventsDetailsFragment.java
+++ app/src/main/java/views/pages/EventsDetailsFragment.java
@@ -7,13 +7,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.TextView;
 
 import com.meetmyage.com.meetmyageapp.R;
+
 
 public class EventsDetailsFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
-
+    LayoutInflater mInflater;
     public EventsDetailsFragment() {
         // Required empty public constructor
     }
@@ -27,7 +30,26 @@ public class EventsDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_events_details, container, false);
+        mInflater = inflater;
+        View myView  = inflater.inflate(R.layout.fragment_events_details, container, false);
+        addEventsTiles(myView);
+        return myView;
+    }
+
+    private void addEventsTiles(View pView) {
+        GridLayout myGrid = pView.findViewById(R.id.eventsFragmentGrid);
+        for (int i=0;i<10;i++) {
+            View myView = mInflater.inflate(R.layout.fragment_event_tile, null);
+            TextView myPeopleGoing = myView.findViewById(R.id.event_details_peopleGoing);
+            TextView myEventName = myView.findViewById(R.id.event_details_holder_image_event_name);
+            TextView myEventDesc = myView.findViewById(R.id.event_details_holder_image_event_desc);
+            TextView myEventLocation = myView.findViewById(R.id.event_details_holder_viewMore);
+            myPeopleGoing.setText(i+" people going");
+            myEventLocation.setText("Flat 402, Vishnu Sapphire, Kondapur, Hyderabad, 500084");
+            myEventDesc.setText(" hello hello hello hello please join hellp join");
+            myEventName.setText(" Test Event "+i);
+            myGrid.addView(myView);
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
